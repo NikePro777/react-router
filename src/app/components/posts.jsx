8f7b84fc466fb8ffa2bb83d36d6bb3efef497de1
evgeniy.datskiy <evgenij.datskij@mail.ru>
2021-12-20 01:00:11 +0500

    lesson finished

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,25 +1,27 @@
 import Post from "./post";
 import PostsList from "./postsList";
-import query from "query-string";
-import _ from "lodash";
-const Posts = ({ match, location }) => {
+import { useParams } from "react-router-dom";
+// import query from "query-string";
+// import _ from "lodash";
+const Posts = ({ match }) => {
   const posts = [
     { id: 1, label: "post1" },
     { id: 2, label: "post2" },
     { id: 3, label: "post3" },
   ];
-  const search = query.parse(location.search);
-  console.log(search);
-  const cropPosts = search
-    ? _(posts).slice(0).take(search.count).value()
-    : posts;
-  const postId = match.params.postId;
+  const params = useParams();
+  // const search = query.parse(location.search);
+  // console.log(search);
+  // const cropPosts = search
+  //   ? _(posts).slice(0).take(search.count).value()
+  //   : posts;
+  const { postId } = params;
   return (
     <>
       {postId ? (
         <Post posts={posts} id={postId} />
       ) : (
-        <PostsList posts={cropPosts} />
+        <PostsList posts={posts} />
       )}
     </>
   );
