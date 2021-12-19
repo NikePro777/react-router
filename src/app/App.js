f18abc305468d560d6c107cfb9a0a3ee1135fa3f
evgeniy.datskiy <evgenij.datskij@mail.ru>
2021-12-19 19:48:59 +0500

    routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,27 @@
 import NavBar from "./components/navBar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Home from "./components/home";
 import Login from "./components/login";
 import Posts from "./components/posts";
 import Stats from "./components/stats";
+import NotFound from "./components/not-found";
 
 function App() {
   return (
     <div>
       <NavBar />
       <h1>App</h1>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard/stats" element={<Stats />} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/stats" component={Stats} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts/:postId?" component={Posts} />
+        <Route path="/404" component={NotFound} />
+        <Redirect path="/admin" to="/dashboard" />
+        <Redirect to="/404" />
+      </Switch>
     </div>
   );
 }
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,5 +1,28 @@
-const Posts = () => {
-  return <h1>Posts</h1>;
+import Post from "./post";
+import PostsList from "./postsList";
+import query from "query-string";
+import _ from "lodash";
+const Posts = ({ match, location }) => {
+  const posts = [
+    { id: 1, label: "post1" },
+    { id: 2, label: "post2" },
+    { id: 3, label: "post3" },
+  ];
+  const search = query.parse(location.search);
+  console.log(search);
+  const cropPosts = search
+    ? _(posts).slice(0).take(search.count).value()
+    : posts;
+  const postId = match.params.postId;
+  return (
+    <>
+      {postId ? (
+        <Post posts={posts} id={postId} />
+      ) : (
+        <PostsList posts={cropPosts} />
+      )}
+    </>
+  );
 };
 
 export default Posts;
